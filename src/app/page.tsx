6bc3536dde76fd9b847acba819197e473de0da91
innocentdavid <paulinnocent04@gmail.com>
2024-09-19 19:18:48 +0100

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,12 @@ import ExpandableCardDemo from "@/components/blocks/expandable-card-demo-grid";
 import ExpandableCardDemoStandard from "@/components/blocks/expandable-card-demo-standard";
 import BackgroundBeamsWithCollisionDemo from "@/components/homepage/background-beams-with-collision-demo";
 import HoverBorderGradientDemo from "@/components/homepage/hover-border-gradient-demo";
-import { PlaceholdersAndVanishInputDemo } from "@/components/homepage/PlaceholdersAndVanishInputDemo";
 
 export default function Home() {
   return (
     <>
     navbar
       <BackgroundBeamsWithCollisionDemo />
-      <br />
-      <br />
-      <br />
-      <PlaceholdersAndVanishInputDemo />
       <br />
       <br />
       <br />
