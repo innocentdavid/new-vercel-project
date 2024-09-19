87b682237cc9d518a88a3eeffb5806c6caf3f9ba
innocentdavid <paulinnocent04@gmail.com>
2024-09-19 13:37:32 +0100

    fixes

--- src/components/ui/placeholders-and-vanish-input.tsx
+++ src/components/ui/placeholders-and-vanish-input.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 "use client";
 
 import { AnimatePresence, motion } from "framer-motion";
@@ -70,9 +71,9 @@ export function PlaceholdersAndVanishInput({
     const newData: any[] = [];
 
     for (let t = 0; t < 800; t++) {
-      let i = 4 * t * 800;
+      const i = 4 * t * 800;
       for (let n = 0; n < 800; n++) {
-        let e = i + 4 * n;
+        const e = i + 4 * n;
         if (
           pixelData[e] !== 0 &&
           pixelData[e + 1] !== 0 &&
